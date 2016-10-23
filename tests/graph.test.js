f0ee60acc96e933758d8cf4adf37e27d020ef121
Jahed <jahed.public@gmail.com>
2016-10-23 17:19:57 +0100

    Capitalise NodeLabels to be more inline with type naming conventions. Added test to ensure NodeLabels exist. Renamed shapes, points and layers so their relationship to maps is more obvious.

--- tests/graph.test.js
+++ tests/graph.test.js
@@ -3,18 +3,46 @@ const file = require('file')
 const path = require('path')
 const assert = require('assert')
 
-describe('Graph', () => {
-    it('should match node filenames with IDs', () => {
-        file.walkSync(path.resolve('./graph'), (dirPath, dirs, filePaths) => {
-            filePaths
-                .filter(filePath => path.extname(filePath) === '.json')
-                .forEach(filePath => {
-                    const fileContent = fs.readFileSync(path.resolve(dirPath, filePath), { encoding: 'utf8' })
-                    const node = JSON.parse(fileContent)
+const graphPath = './graph'
 
-                    assert.strictEqual(node.metadata.id, path.basename(filePath, '.json'))
+let graphObjects
+file.walkSync(path.resolve(graphPath), (dirPath, dirs, filePaths) => {
+    graphObjects = filePaths
+        .filter(filePath => path.extname(filePath) === '.json')
+        .map(filePath => {
+            const absoluteFilePath =  path.resolve(dirPath, filePath)
+            const fileContent = fs.readFileSync(absoluteFilePath, { encoding: 'utf8' })
+            const content = JSON.parse(fileContent)
+            return {
+                absoluteFilePath,
+                content
+            }
+        })
+})
+
+describe(`Graph (${graphObjects.length} objects)`, () => {
+
+    it('object filenames should match assigned IDs', () => {
+        graphObjects.forEach(graphObject => {
+            assert.strictEqual(graphObject.content.metadata.id, path.basename(graphObject.absoluteFilePath, '.json'))
+        })
+    })
+
+    it('labels assigned to nodes should be defined', () => {
+        graphObjects.forEach(graphObject => {
+            if(graphObject.content.metadata.labels) {
+                graphObject.content.metadata.labels.forEach(label => {
+                    assert.doesNotThrow(
+                        () => {
+                            fs.accessSync(
+                                path.resolve(graphPath, 'nodeLabels', `${label}.json`),
+                                fs.constants.F_OK
+                            )
+                        },
+                        `Node(${graphObject.content.metadata.id}) has unknown Label(${label})`
+                    )
                 })
-
+            }
         })
     })
 })
