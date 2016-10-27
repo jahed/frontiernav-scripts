5c72af59d12608799679ea5a00811867e1f2fc20
Jahed <jahed.public@gmail.com>
2016-10-27 23:32:28 +0100

    Merged in Italian localisation strings from frontiernav-i18n.

--- scripts/merge18n.js
+++ scripts/merge18n.js
@@ -1,9 +1,11 @@
 const _ = require('lodash')
 const transformAll = require('./helpers/transformAll')
 const germanNodes = require('../../frontiernav-i18n/locales/graph/nodes.de.json')
+const italianNodes = require('../../frontiernav-i18n/locales/graph/nodes.it.json')
 
 transformAll('./graph', '.json', node => {
     addLocale('de', node, germanNodes)
+    addLocale('it', node, italianNodes)
     return node
 })
 
@@ -11,9 +13,9 @@ function addLocale(locale, node, localeNodes) {
     if(localeNodes[node.id]) {
         const localeNode = localeNodes[node.id]
         if(localeNode.data.name !== node.data.name) {
-            node.locale = {
+            node.locale = Object.assign({}, node.locale, {
                 [locale]: localeNode.data
-            }
+            })
         }
     }
 }
