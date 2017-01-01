d4cec86933d36ffa141e104a66c8658a610a3c34
Jahed <jahed.public@gmail.com>
2017-01-01 14:51:06 +0000

    Added zygarde cell map links

---
+++ scripts/pokemon/addMarkerRelationships.js
@@ -0,0 +1,37 @@
+const readGraph = require('../helpers/readGraph')
+const writeGraph = require('../helpers/writeGraph')
+const changeNodeId = require('../helpers/changeNodeID')
+const _ = require('lodash')
+
+function arrayToIdMap(rawGraph) {
+    return _.mapValues(rawGraph, objs => _.keyBy(objs, 'id'))
+}
+
+const graphDir = './games/pokemon-sun-moon/graph'
+const graph = arrayToIdMap(readGraph(graphDir, ({ content }) => content))
+const encounterRegex = /^encounter-zygarde-cell-(\d+)$/
+
+let newGraph = graph
+_(graph.nodes).forEach(node => {
+    if(encounterRegex.test(node.id)) {
+        const matches = encounterRegex.exec(node.id)
+        const number = matches[1]
+        if(number) {
+            const markerId = `marker-zygarde-cell-${number}`
+            const rid = `${markerId}__MAP_LINK__${node.id}`
+            newGraph = Object.assign({}, newGraph, {
+                relationships: Object.assign({}, newGraph.relationships, {
+                    [rid]: {
+                        id: rid,
+                        start: markerId,
+                        type: 'MAP_LINK',
+                        end: node.id
+                    }
+                })
+            })
+        }
+    }
+})
+
+
+writeGraph(graphDir, newGraph, { dryRun: false })
