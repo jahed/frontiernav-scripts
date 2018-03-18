18aebe11c327abf023648f272cb103828447e7f3
Jahed <jahed.public@gmail.com>
2018-03-18 00:52:15 +0000

    chore: Added linting.

--- packages/table-to-xsv/bin/tableToXSV.cli.js
+++ packages/table-to-xsv/bin/tableToXSV.cli.js
@@ -5,35 +5,35 @@ const tableToXSV = require('../src/tableToXSV')
 const argv = require('minimist')(process.argv.slice(2))
 
 const {
-    _: [input],
-    output,
-    separator = '\t',
-    help
+  _: [input],
+  output,
+  separator = '\t',
+  help
 } = argv
 
-if(help || !input) {
-    console.log()
-    console.log('TABLE TO XSV')
-    console.log('  Converts a HTML <table> to a X-separated value string.')
-    console.log()
-    console.log('USAGE')
-    console.log(`  node ${path.relative(process.cwd(), __filename)} [options] <path to html>`)
-    console.log()
-    console.log('OPTIONS')
-    console.log('  --separator <string>  : The separator to use. (default: \\t)')
-    console.log('  --output <path>       : Output file. (default: stdout)')
-    console.log()
-    return
+if (help || !input) {
+  console.log()
+  console.log('TABLE TO XSV')
+  console.log('  Converts a HTML <table> to a X-separated value string.')
+  console.log()
+  console.log('USAGE')
+  console.log(`  node ${path.relative(process.cwd(), __filename)} [options] <path to html>`)
+  console.log()
+  console.log('OPTIONS')
+  console.log('  --separator <string>  : The separator to use. (default: \\t)')
+  console.log('  --output <path>       : Output file. (default: stdout)')
+  console.log()
+  process.exit(help ? 0 : 1)
 }
 
 const html = fs.readFileSync(path.resolve(input))
 
 const result = tableToXSV({ separator })(html)
 
-if(output) {
-    const resolvedOutput = path.resolve(argv.output)
-    console.log(`Writing to ${resolvedOutput}`)
-    fs.writeFileSync(resolvedOutput, result)
+if (output) {
+  const resolvedOutput = path.resolve(argv.output)
+  console.log(`Writing to ${resolvedOutput}`)
+  fs.writeFileSync(resolvedOutput, result)
 } else {
-    console.log(result)
+  console.log(result)
 }
--- packages/table-to-xsv/src/tableToXSV.js
+++ packages/table-to-xsv/src/tableToXSV.js
@@ -1,23 +1,23 @@
 const cheerio = require('cheerio')
 
-function tableToXSV({ separator }) {
-    return html => {
-        const $ = cheerio.load(html)
-        const table = $('table')
-        return table.find('tr')
-            .get()
-            .map(row => $(row)
-                .find('th, td')
-                .get())
-            .map(columns => columns
-                .map(column => $(column)
-                    .text())
-                .map(text => text.replace(/"/g, '""'))
-                .map(text => `"${text}"`)
-                .join(separator)
-            )
-            .join('\n')
-    }
+function tableToXSV ({ separator }) {
+  return html => {
+    const $ = cheerio.load(html)
+    const table = $('table')
+    return table.find('tr')
+      .get()
+      .map(row => $(row)
+        .find('th, td')
+        .get())
+      .map(columns => columns
+        .map(column => $(column)
+          .text())
+        .map(text => text.replace(/"/g, '""'))
+        .map(text => `"${text}"`)
+        .join(separator)
+      )
+      .join('\n')
+  }
 }
 
 module.exports = tableToXSV
