7cbbddfba8000d2bf9d2230cc12c4ffdd263ee3c
Jahed Ahmed <jahed.public@gmail.com>
2018-10-27 14:35:51 +0100

    feat(build-data): moved build-data into this repo from frontiernav

---
+++ scripts/logger.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const pino = require('pino')
+
+const logger = pino({
+  prettyPrint: {
+    translateTime: "yyyy-mm-dd'T'HH:MM:ss'Z'"
+  },
+  serializers: {
+    error: pino.stdSerializers.err
+  }
+})
+
+exports.module = filename => logger.child({ name: path.basename(filename, '.js') })
