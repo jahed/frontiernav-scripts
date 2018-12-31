8854e0a660a7a208d04f3afbae15647540260dda
Jahed Ahmed <jahed.public@gmail.com>
2018-12-31 17:07:22 +0000

    fix(xc2): process FirstNamed drops separately from regular item drops

--- extractors/xenoblade-chronicles-2/src/entities/EnemyDropTables.js
+++ extractors/xenoblade-chronicles-2/src/entities/EnemyDropTables.js
@@ -12,35 +12,43 @@ const getDrops = async dropTable => {
   const data = [
     {
       id: dropTable.ItemID1,
-      rate: dropTable.DropProb1
+      rate: dropTable.DropProb1,
+      first_unique_kill_drop: dropTable.FirstNamed1 === 1
     },
     {
       id: dropTable.ItemID2,
-      rate: dropTable.DropProb2
+      rate: dropTable.DropProb2,
+      first_unique_kill_drop: dropTable.FirstNamed2 === 1
     },
     {
       id: dropTable.ItemID3,
-      rate: dropTable.DropProb3
+      rate: dropTable.DropProb3,
+      first_unique_kill_drop: dropTable.FirstNamed3 === 1
     },
     {
       id: dropTable.ItemID4,
-      rate: dropTable.DropProb4
+      rate: dropTable.DropProb4,
+      first_unique_kill_drop: dropTable.FirstNamed4 === 1
     },
     {
       id: dropTable.ItemID5,
-      rate: dropTable.DropProb5
+      rate: dropTable.DropProb5,
+      first_unique_kill_drop: dropTable.FirstNamed5 === 1
     },
     {
       id: dropTable.ItemID6,
-      rate: dropTable.DropProb6
+      rate: dropTable.DropProb6,
+      first_unique_kill_drop: dropTable.FirstNamed6 === 1
     },
     {
       id: dropTable.ItemID7,
-      rate: dropTable.DropProb7
+      rate: dropTable.DropProb7,
+      first_unique_kill_drop: dropTable.FirstNamed7 === 1
     },
     {
       id: dropTable.ItemID8,
-      rate: dropTable.DropProb8
+      rate: dropTable.DropProb8,
+      first_unique_kill_drop: dropTable.FirstNamed8 === 1
     }
   ]
 
@@ -52,7 +60,8 @@ const getDrops = async dropTable => {
           .then(name => nameToId(name))
           .then(id => ({
             id,
-            rate: drop.rate
+            rate: drop.rate,
+            first_unique_kill_drop: drop.first_unique_kill_drop
           }))
           .catch(() => null)
       })
@@ -60,28 +69,51 @@ const getDrops = async dropTable => {
     .then(drops => drops
       .filter(d => d)
       .reduce((acc, next) => {
-        const existing = acc[next.id]
+        if (next.first_unique_kill_drop) {
+          const existing = acc.first_unique_kill_drops[next.id]
+          const nextRate = { rate: 1000 }
+          if (existing) {
+            existing.rates.push(nextRate)
+          } else {
+            acc.first_unique_kill_drops[next.id] = {
+              id: next.id,
+              rates: [nextRate]
+            }
+          }
+        }
+
+        if (next.rate === 0) {
+          return acc
+        }
+
+        const existing = acc.drops[next.id]
         const nextRate = { rate: next.rate }
         if (existing) {
           existing.rates.push(nextRate)
         } else {
-          acc[next.id] = {
+          acc.drops[next.id] = {
             id: next.id,
             rates: [nextRate]
           }
         }
         return acc
-      }, {})
+      }, {
+        drops: {},
+        first_unique_kill_drops: {}
+      })
     )
-    .then(dict => _.values(dict))
+    .then(dict => ({
+      drops: _.values(dict.drops),
+      first_unique_kill_drops: _.values(dict.first_unique_kill_drops)
+    }))
 }
 
 const toEntity = _.memoize(async raw => {
-  const drops = await getDrops(raw)
-
+  const { drops, first_unique_kill_drops } = await getDrops(raw)
   return {
     name: `Enemy Drop Table #${raw.id}`,
-    drops
+    drops,
+    first_unique_kill_drops
   }
 }, raw => raw.id)
 
